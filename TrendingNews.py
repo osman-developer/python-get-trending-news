8734dc0501a28a7f6ab0cb5672bd3b93a8034cb6
osman-developer <57558743+osman-developer@users.noreply.github.com>
2021-05-10 17:35:21 +0300

    changes

--- TrendingNews.py
+++ TrendingNews.py
@@ -4,3 +4,10 @@ googlenews = GoogleNews()
 googlenews = GoogleNews(period='7d')
 googlenews.search('USA')
 result = googlenews.result()
+
+for x in result:
+	print("-"*50)
+	print("Title--",x['title'])
+	print("Date/Time--",x['date'])
+	print("Description--",x['desc'])
+	print("Link--",x[])
